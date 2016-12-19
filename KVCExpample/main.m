945608526ff9e738c6b1d359c09903bad259b412
Ильдар Залялов <Zalyalov.Ild@gmail.com>
2016-12-19 13:47:57 +0300

    Initial Commit

---
+++ KVCExpample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVCExpample
+//
+//  Created by Ildar Zalyalov on 19.12.16.
+//  Copyright © 2016 com.itis.iosLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
